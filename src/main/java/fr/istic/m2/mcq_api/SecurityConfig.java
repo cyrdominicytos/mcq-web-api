3724a3bfb749dde2e942926bf2a6aaffd9b965b2
Tossou Cyriaque <cyriaque.tossou@etudiant.univ-rennes1.fr>
2024-05-22 06:38:58 +0200

    feat: swagger doc

--- src/main/java/fr/istic/m2/mcq_api/SecurityConfig.java
+++ src/main/java/fr/istic/m2/mcq_api/SecurityConfig.java
@@ -1,4 +1,4 @@
-package fr.istic.m2.mcq_api.security;
+package fr.istic.m2.mcq_api;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +14,7 @@ import org.springframework.web.filter.CorsFilter;
  */
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
---
+++ src/main/java/fr/istic/m2/mcq_api/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package fr.istic.m2.mcq_api;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The class to configure OpenAPI documentation, to describe the REST API.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("QCM APP - API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
