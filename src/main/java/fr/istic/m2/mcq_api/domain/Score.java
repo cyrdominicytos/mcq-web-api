50b117ae3b070b6e92ec2f57d57e5c33f451f247
Tuo Adama <tuoadama17@gmail.com>
2024-05-28 20:40:07 +0200

    feat: qcm score

---
+++ src/main/java/fr/istic/m2/mcq_api/domain/Score.java
@@ -0,0 +1,18 @@
+package fr.istic.m2.mcq_api.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Score {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Qcm qcm;
+    @ManyToOne
+    private Student student;
+    private Integer validAnswer;
+    private Integer totalValidAnswer;
+}
